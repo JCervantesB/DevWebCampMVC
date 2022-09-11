9378e21e7dd2d4cc80564f9ba7847ed430d1c548
imjcervantes <imjcervantes@gmail.com>
2022-09-11 12:34:48 -0600

    Finalizado el footer

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -9,22 +9,22 @@
         </div>
 
         <nav class="menu-redes">
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://facebook.com/C%C3%B3digo-Con-Juan-103341632130628">
                 <span class="menu-redes__ocultar">Facebook</span>
             </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://twitter.com/codigoconjuan">
                 <span class="menu-redes__ocultar">Twitter</span>
             </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://youtube.com/codigoconjuan">
                 <span class="menu-redes__ocultar">YouTube</span>
             </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://instagram.com/codigoconjuan">
                 <span class="menu-redes__ocultar">Instagram</span>
             </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://tiktok.com/@codigoconjuan">
                 <span class="menu-redes__ocultar">Tiktok</span>
             </a>
-            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://github.com/JCervantesB">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://github.com/codigoconjuan">
                 <span class="menu-redes__ocultar">Github</span>
             </a>
         </nav>
