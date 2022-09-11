6b2eef21ed3d2c8db3fd64e6268b5e6abf6838e2
imjcervantes <imjcervantes@gmail.com>
2022-09-11 12:23:05 -0600

    Creando el Footer

---
+++ views/templates/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+    <div class="footer__grid">
+        <div class="footer__contenido">
+            <h3 class="footer__logo">&#60;DevWebcamp&#62;</h3>
+
+            <p class="footer__texto">
+                DevWebcamp es una conferencia para todos los desarrolladores de todos los niveles, se lleva a cabo de forma presencial y en línea.
+            </p>
+        </div>
+
+        <nav class="menu-redes">
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+                <span class="menu-redes__ocultar">Facebook</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+                <span class="menu-redes__ocultar">Twitter</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+                <span class="menu-redes__ocultar">YouTube</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+                <span class="menu-redes__ocultar">Instagram</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="#">
+                <span class="menu-redes__ocultar">Tiktok</span>
+            </a>
+            <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://github.com/JCervantesB">
+                <span class="menu-redes__ocultar">Github</span>
+            </a>
+        </nav>
+    </div>
+
+    <p class="footer__copyright">
+        DevWebcamp
+        <span class="footer__copyright--regular">
+            - Todos los derechos reservados <?php echo date('Y'); ?>
+        </span>
+    </p>
+</footer>
