ba5f0d55c2e646d006fe1996292bef66265287c4
imjcervantes <imjcervantes@gmail.com>
2022-09-14 17:00:45 -0600

    Finalizada la autentificación

---
+++ views/auth/mensaje.php
@@ -0,0 +1,7 @@
+<main class="auth">
+    <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+    <p class="auth__texto">Cuenta Creada Exitosamente</p>
+
+    <p>Es necesario confirmar tu cuenta, revisa tu bandeja de entrada para hacerlo</p>
+
+</main>
--- views/auth/reestablecer.php
+++ views/auth/reestablecer.php
@@ -1,24 +1,25 @@
 <main class="auth">
     <h2 class="auth__heading"><?php echo $titulo; ?></h2>
-    <p class="auth__texto">Recupera tu acceso a DevWebcamp</p>
+    <p class="auth__texto">Coloca tu Nuevo Passowrd DevWebcamp</p>
 
     <?php
         include_once __DIR__ . '/../templates/alertas.php';
     ?>
-
-    <form method="POST" action="/olvide" class="formulario">
+    <?php if($token_valido) { ?>
+    <form method="POST" class="formulario">
         <div class="formulario__campo">
-            <label for="email" class="formulario__label">Email</label>
+            <label for="password" class="formulario__label">Nuevo Password</label>
             <input 
-                type="email"
+                type="password"
                 class="formulario__input"
-                placeholder="Tu Email"
-                id="email"
-                name="email"
+                placeholder="Ingresa un Password"
+                id="password"
+                name="password"
             >
         </div>
-        <input type="submit" class="formulario__submit" value="Enviar Instrucciones">
+        <input type="submit" class="formulario__submit" value="Guardar Password">
     </form>
+    <?php } ?>
 
     <div class="acciones">
         <a href="/login" class="acciones__enlace">¿Ya tienes una cuenta? Inicia Sesión</a>
